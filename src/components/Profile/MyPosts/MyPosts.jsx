60c93c59d079b54e80264ca3095fccca2fee7d41
Dmitriy <dm.hmara@gmail.com>
2023-01-13 14:43:08 +0200

    28

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,6 +4,7 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
+  
     let posts = [
         {id: 1, message: 'Hi, how are you?', likesCount: 12},
         {id: 2, message: 'It\'s my first post', likesCount: 11},
