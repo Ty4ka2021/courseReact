0b79152c7da58a5fdadfc4e7188ae30deea2f7ab
Dmitriy <dm.hmara@gmail.com>
2023-01-29 19:52:06 +0200

    61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-const Header = () => {
-    return <header className={s.header}>
-        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
-    </header>
+const Header = (props) => {
+  return <header className={s.header}>
+    <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+
+    <div className={s.loginBlock}>
+      {props.isAuth 
+      ? props.login
+      : <NavLink to={'/login'}>Login</NavLink>
+      }
+    </div>
+  </header>
 }
 
 export default Header;
